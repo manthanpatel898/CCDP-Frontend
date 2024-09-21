067c30e350c24c02ef8be5c748bb2bb6767d0bfa
manthanpatel898 <134184638+manthanpatel898@users.noreply.github.com>
2024-09-21 20:29:05 +0530

    remove unwanted code

--- src/config/config.ts
+++
@@ -1,6 +0,0 @@
-export const config = {
-    pageLimit: 10,
-    apiBaseUrl: process.env.REACT_APP_API_URL,
-    defaultLanguage: 'en',
-    languages: ['en', 'es'],
-  }
--- src/routing/layout/PrivateLayout.tsx
+++
@@ -1,5 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-export default function PrivateLayout() {
-  const token: string | null = localStorage.getItem("token");
-  return token ? <Outlet /> : <Navigate to="/login" />;
-}
